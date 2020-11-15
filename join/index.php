ee47413a921c71614952f4f45279fb4d35b750dc
hiroshima820 <53898556+hiroshima820@users.noreply.github.com>
2020-11-15 15:29:35 +0900

    データベース保存、重複登録の防止

--- join/index.php
+++ join/index.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+require('../dbconnect.php');
 
 if (!empty($_POST)) {
 	if ($_POST['name'] === '') {
@@ -19,6 +20,16 @@ if (!empty($_POST)) {
 		$ext = substr($fileName, -3);
 		if ($ext != 'jpg' && $ext != 'gif' && $ext != 'png') {
 			$error['image'] = 'type';
+		}
+	}
+
+	// アカウントの重複をチェック
+	if (empty($error)) {
+		$member = $db->prepare('SELECT COUNT(*) AS cnt FROM members WHERE email=?');
+		$member->execute(array($_POST['email']));
+		$record = $member->fetch();
+		if ($record['cnt'] > 0) {
+			$error['email'] = 'duplicate';
 		}
 	}
 
@@ -73,6 +84,9 @@ if ($_REQUEST['action'] == 'rewrite' && isset($_SESSION['join'])) {
 						<?php if ($error['email'] === 'blank') : ?>
 							<p class="error">＊ メールアドレスを入力してください</p>
 						<?php endif; ?>
+						<?php if ($error['email'] === 'duplicate') : ?>
+							<p class="error">＊ 指定されたメールアドレスは、すでに登録されています</p>
+						<?php endif; ?>
 					</dd>
 					<dt>パスワード<span class="required">必須</span></dt>
 					<dd>
@@ -90,7 +104,7 @@ if ($_REQUEST['action'] == 'rewrite' && isset($_SESSION['join'])) {
 						<?php if ($error['image'] === 'type') : ?>
 							<p class="error">＊ 写真などは「.jpg」「.gif」「.png」の画像を指定してください</p>
 						<?php endif; ?>
-						<?php if (!empty($error)): ?>
+						<?php if (!empty($error)) : ?>
 							<p class="error">＊ 恐れ入りますが、画像を改めて指定してください</p>
 						<?php endif; ?>
 					</dd>
